a061597164cc70ce5d428f6be798af08eff5bac4
Matricarin <matricarin@gmail.com>
2025-03-02 20:06:07 +0500

    new service was added

---
+++ ContactsList.UI/Data/IPersonDataService.cs
@@ -0,0 +1,8 @@
+﻿using ContactsList.Model;
+
+namespace ContactsList.UI.Data;
+
+public interface IPersonDataService
+{
+    IEnumerable<Person> GetAll();
+}
---
+++ ContactsList.UI/Data/PersonDataService.cs
@@ -0,0 +1,14 @@
+﻿using ContactsList.Model;
+
+namespace ContactsList.UI.Data;
+
+public class PersonDataService : IPersonDataService
+{
+    public IEnumerable<Person> GetAll()
+    {
+        yield return new Person()
+        {
+            Id = 1, FirstName = "Alexander", LastName = "Goldman", Email = "ga@mail.com", PhoneNumber = "+799988855"
+        };
+    }
+}
