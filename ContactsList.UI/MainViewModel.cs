04edc90b47b5146d5ad1d655645d433dfa8b7342
Matricarin <matricarin@gmail.com>
2025-03-02 20:24:37 +0500

    viewmodel was added

---
+++ ContactsList.UI/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using ContactsList.Model;
+using ContactsList.UI.Data;
+
+namespace ContactsList.UI;
+
+public class MainViewModel
+{
+    private IPersonDataService _personDataService;
+
+    public ObservableCollection<Person> StoredPersonsList { get; }
+
+    public MainViewModel(IPersonDataService personDataService)
+    {
+        StoredPersonsList = new ObservableCollection<Person>();
+        _personDataService = personDataService;
+    }
+
+    public void LoadPersons()
+    {
+        var persons = _personDataService.GetAll();
+        StoredPersonsList.Clear();
+        foreach (var person in persons)
+        {
+            StoredPersonsList.Add(person);
+        }
+    }
+
+    private Person _selectedPerson;
+    
+    public Person SelectedPerson
+    {
+        get { return _selectedPerson;}
+        set { _selectedPerson = value; }
+    }
+}
