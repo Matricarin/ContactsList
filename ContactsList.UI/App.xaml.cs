c2c1af97891c726fdd6d2c4d87b6f6c55826ad90
Matricarin <matricarin@gmail.com>
2025-03-05 17:47:48 +0500

    starup logic was changed

--- ContactsList.UI/App.xaml.cs
+++ ContactsList.UI/App.xaml.cs
@@ -1,10 +1,16 @@
 ﻿using System.Windows;
+using ContactsList.UI.Data;
+using ContactsList.UI.ViewModels;
 
 namespace ContactsList.UI;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
+    private void Application_OnStartUp(object sender, StartupEventArgs e)
+    {
+        var personDataService = new PersonDataService();
+        var viewModel = new MainViewModel(personDataService);
+        var window = new MainWindow(viewModel);
+        window.Show();
+    }
 }
