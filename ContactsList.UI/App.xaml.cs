3e088f7ff93188471c568e456a06862edc1a1816
Matricarin <matricarin@gmail.com>
2025-03-14 23:50:38 +0500

    dependency injection was added

--- ContactsList.UI/App.xaml.cs
+++ ContactsList.UI/App.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System.Windows;
-using ContactsList.UI.Data;
-using ContactsList.UI.ViewModels;
+using Autofac;
+using ContactsList.UI.StartUp;
 
 namespace ContactsList.UI;
 
@@ -8,9 +8,9 @@ public partial class App : Application
 {
     private void Application_OnStartUp(object sender, StartupEventArgs e)
     {
-        var personDataService = new PersonDataService();
-        var viewModel = new MainViewModel(personDataService);
-        var window = new MainWindow(viewModel);
+        var bootstrapper = new Bootstrapper();
+        var container = bootstrapper.Bootstrap();
+        var window = container.Resolve<MainWindow>();
         window.Show();
     }
 }
---
+++ ContactsList.UI/StartUp/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using ContactsList.UI.Data;
+using ContactsList.UI.ViewModels;
+
+namespace ContactsList.UI.StartUp;
+
+public sealed class Bootstrapper
+{
+    public IContainer Bootstrap()
+    {
+        var builder = new ContainerBuilder();
+        builder.RegisterType<MainWindow>().AsSelf();
+        builder.RegisterType<MainViewModel>().AsSelf();
+        builder.RegisterType<PersonDataService>().As<IPersonDataService>();
+        return builder.Build();
+    }
+}
